04ded6f6256d57aaad55414ecb407fd0d43dbc22
phil85 <philipp.baumann@pqm.unibe.ch>
2024-06-09 17:20:42 +0200

    Add table with results on noisy constraint sets

--- README.md
+++ README.md
@@ -51,10 +51,6 @@ On this repository, we provide the detailed computational results of the PCCC-Al
 
 #### Results on noisy constraint sets:
 
-| Metric                                 | 5% CS                                     | 10% CS                                     | 15% CS                                     | 20% CS                                     |
-|:---------------------------------------|-------------------------------------------|--------------------------------------------|--------------------------------------------|--------------------------------------------|
-| ARI                                    | [Table W25](tables/ARI-COL1-5.pdf)        | [Table W26](tables/ARI-COL1-10.pdf)        | [Table W27](tables/ARI-COL1-15.pdf)        | [Table W28](tables/ARI-COL1-20.pdf)        |
-| Inertia                                | [Table W29](tables/Inertia-COL1-5.pdf)    | [Table W30](tables/Inertia-COL1-10.pdf)    | [Table W31](tables/Inertia-COL1-15.pdf)    | [Table W32](tables/Inertia-COL1-20.pdf)    |
-| Silhouette                             | [Table W33](tables/Silhouette-COL1-5.pdf) | [Table W34](tables/Silhouette-COL1-10.pdf) | [Table W35](tables/Silhouette-COL1-15.pdf) | [Table W36](tables/Silhouette-COL1-20.pdf) |
-| Violations of cannot-link constraints  | [Table W37](tables/Violations-COL1-5.pdf) | [Table W38](tables/Violations-COL1-10.pdf) | [Table W39](tables/Violations-COL1-15.pdf) | [Table W40](tables/Violations-COL1-20.pdf) |
-| Running time                           | [Table W41](tables/CPU-COL1-5.pdf)        | [Table W42](tables/CPU-COL1-10.pdf)        | [Table W43](tables/CPU-COL1-15.pdf)        | [Table W44](tables/CPU-COL1-20.pdf)        |
+| Metric|5% CS | 10% CS | 15% CS | 20% CS | 
+|:-----|-----|-----|-----|-----|
+| ARI| [Table W114](tables/ARI-COL1-5.pdf)| [Table W115](tables/ARI-COL1-10.pdf)| [Table W116](tables/ARI-COL1-15.pdf)| [Table W117](tables/ARI-COL1-20.pdf)||
---
+++ compile_tables_noisy.py
@@ -0,0 +1,100 @@
+import os
+
+
+def create_file(latex_file_name, metric, folder, collection, constraint_set, caption, table_counter):
+    with open('latex_files/' + latex_file_name, 'w') as f:
+        f.write(r'% !TeX spellcheck = en_US' + '\n')
+        f.write(r'\documentclass[twoside,11pt]{article}' + '\n')
+        f.write(r'\usepackage{tabularx}' + '\n')
+        f.write(r'\usepackage{booktabs}' + '\n')
+        f.write(r'\usepackage[margin=50pt]{geometry}' + '\n\n')
+
+        f.write('\makeatletter' + '\n')
+        f.write('\setlength\@fptop{0\p@}' + '\n')
+        f.write('\makeatother' + '\n\n')
+
+        f.write(r'\begin{document}' + '\n')
+        f.write(r'\renewcommand*\thetable{W\arabic{table}}' + '\n')
+
+        f.write(r'\setcounter{table}{' + str(table_counter) + '}' + '\n')
+
+        f.write(r'\begin{table}' + '\n')
+        f.write(r'\input{' + '../tbls/{:s}/tbl_{:s}_comparison_noisy_constraint_sets_{:g}'.format(folder, metric,
+                                                                                 constraint_set) + '}' + '\n')
+        f.write(r'\caption{' + caption + '}' + '\n')
+        f.write(r'\end{table}' + '\n')
+
+        f.write('\n\n')
+
+        f.write(r'\end{document}' + '\n')
+
+
+constraint_sets = [5, 10, 15, 20]
+
+# %% ARI
+
+table_counter = 115
+collection = 'COL1'
+metric_abbreviation = 'ARI'
+metric = 'Average Adjusted Rand Index (ARI) values'
+algorithms = 'of the PCCC algorithms and the state-of-the-art algorithm (CSC)'
+interpretation = 'Higher values indicate more overlap with the ground truth assignment.'
+meaning_bold = 'The highest values are stated in bold.'
+k_means = 'The column KMEANS reports the average ARI values that were obtained with the unconstrained k-means algorithm.'
+meaning_hyphen = 'The hyphen indicates that the respective algorithm returned no solution within the time limit of 1,800 seconds.'
+
+for constraint_set in constraint_sets:
+    latex_file_name = '{:s}-{:s}-{:g}-noisy.tex'.format(metric_abbreviation, collection, constraint_set)
+    constraint_set_str = 'obtained with constraint sets of size {:g}\% CS.'.format(constraint_set)
+    caption = ('{:s} {:s} {:s} {:s} {:s} {:s} {:s}'.format(metric,
+                                                           algorithms,
+                                                           constraint_set_str,
+                                                           interpretation,
+                                                           meaning_bold,
+                                                           k_means,
+                                                           meaning_hyphen))
+
+    create_file(latex_file_name, metric='ari', folder='revision_experiment6', collection='col1',
+                constraint_set=constraint_set, caption=caption, table_counter=table_counter)
+
+    table_counter += 1
+
+    # Compile latex file W1-W4.tex
+    os.system('pdflatex latex_files/' + latex_file_name + ' -output-directory=tables')
+
+    # Delete auxiliary files
+    os.system('rm tables/*.aux')
+    os.system('rm tables/*.log')
+
+# %%
+
+# Open file
+table_counter = 114
+collection = 'COL1'
+metrics = ['ARI']
+
+with open('markdown_table_noisy.txt', 'w') as f:
+
+    # Add header
+    header_str = '| Metric' + '|'
+    for i in constraint_sets:
+        header_str = header_str + str(i) + '% CS' + ' | '
+    header_str += '\n'
+
+    f.write(header_str)
+
+    table_str = '|:-----|'
+    for i in constraint_sets:
+        table_str = table_str + '-----|'
+    table_str += '\n'
+
+    f.write(table_str)
+
+    for metric in metrics:
+        row_str = '| ' + metric + '|'
+        for i in constraint_sets:
+            row_str = row_str + ' [Table W{:d}](tables/'.format(table_counter) + metric + '-' + collection + '-' + str(i) + '.pdf)|'
+            table_counter += 1
+        row_str += '|\n'
+
+        f.write(row_str)
